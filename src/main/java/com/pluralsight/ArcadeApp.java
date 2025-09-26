d5b1aecc6eeba11dcce9cc26d1fa1c176aaf77e8
nati-tewolde <natisnewemail@gmail.com>
2025-09-25 23:38:07 -0400

    refactored code - improvement #1

--- src/main/java/com/pluralsight/ArcadeApp.java
+++ src/main/java/com/pluralsight/ArcadeApp.java
@@ -31,46 +31,78 @@ public class ArcadeApp {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the amount of money you want to spend: $");
-        double moneySpent = input.nextDouble();
+        double amount = input.nextDouble();
         input.nextLine();
 
-        double token = moneySpent / 0.25;
+        int tokens = (int) (amount / 0.25);
 
         System.out.print("1 - Racing (cost: 4 tokens) \n2 - Shooting (cost: 3 tokens) \n3 - Dance (cost: 2 tokens) \n4 - Pinball (cost: 1 token) \nSelect a game: ");
-        String game = input.nextLine();
+        int game = input.nextInt();
 
-        double tokenCost = 0;
+/*      double tokenCost = 0;
         int gameReplay = 0;
         int tokensLeft = 0;
         switch (game.toLowerCase()){ //Initially had switch (game) cases set to integer values corresponding to the game number, issue was needed to store game name to print later on
             case "racing":
                 tokenCost = 4;
-                gameReplay = (int) token / 4;
-                tokensLeft = (int) token % 4;
+                gameReplay = (int) tokens / 4;
+                tokensLeft = (int) tokens % 4;
                 break;
             case "shooting":
                 tokenCost = 3;
-                gameReplay = (int) token / 3;
-                tokensLeft = (int) token % 3;
+                gameReplay = (int) tokens / 3;
+                tokensLeft = (int) tokens % 3;
                 break;
             case "dance":
                 tokenCost = 2;
-                gameReplay = (int) token / 2;
-                tokensLeft = (int) token % 2;
+                gameReplay = (int) tokens / 2;
+                tokensLeft = (int) tokens % 2;
                 break;
             case "pinball":
                 tokenCost = 1;
-                gameReplay = (int) token;
+                gameReplay = (int) tokens;
                 break;
             default:
                 System.out.println(game + " is not a valid game option.");
                 System.exit(0);
+        }*/
+
+        int tokenCost;
+        String gameName;
+        int replays;
+        int leftover = 0;
+        switch (game) {
+            case 1:
+                tokenCost = 4;
+                replays = tokens / tokenCost;
+                leftover = tokens % tokenCost;
+                gameName = "Racing";
+                break;
+            case 2:
+                tokenCost = 3;
+                replays = tokens / tokenCost;
+                leftover = tokens % tokenCost;
+                gameName = "Shooting";
+                break;
+            case 3:
+                tokenCost = 2;
+                replays = tokens / tokenCost;
+                leftover = tokens % tokenCost;
+                gameName = "Dance";
+                break;
+            case 4:
+                tokenCost = 1;
+                replays = tokens;
+                gameName = "Pinball";
+                break;
+            default:
+                System.out.println(game + " is not a valid game option.");
+                return;
         }
-
-
-        System.out.println("You have $" + moneySpent + ", which gives you " + (int) token + " tokens.");
-        System.out.println("You selected " + game + ". Each game costs " + (int) tokenCost + " tokens.");
-        System.out.println("You can play " + gameReplay + " times and will have " + tokensLeft + " token(s) left over.");
+        
+        System.out.println("You have $" + amount + ", which gives you " + tokens + " tokens.");
+        System.out.println("You selected " + gameName + ". Each game costs " + tokenCost + " tokens.");
+        System.out.println("You can play " + replays + " times and will have " + leftover + " token(s) left over.");
 
 
     }
